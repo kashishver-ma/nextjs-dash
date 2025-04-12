8a2387666e1fc813c9f5712fac623ebccfc378d0
kashishver-ma <itskashishverma@gmail.com>
2025-04-12 23:07:45 +0530

    Initial commit2

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function invoices() {
+  return <h1>Hi am invoice</h1>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
+import AcmeLogo from "@/app/ui/acme-logo";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { l } from "@/app/ui/fonts";
+import { inter } from "@/app/ui/fonts";
+import Image from "next/image";
 
 export default function Page() {
   return (
@@ -8,10 +11,24 @@ export default function Page() {
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
+      <Image
+        src="/hero-desktop.png"
+        alt="ss of dahsbaord img "
+        width={1000}
+        height={760}
+        className="hidden md:block"
+      ></Image>
+      <Image
+        src="/hero-mobile.png"
+        alt="ss of mobile dashboard img "
+        width={1000}
+        height={760}
+        className="hidden lg:block sm:block"
+      ></Image>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
